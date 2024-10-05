54f26eecc29d9acfbe2f1f6e066c3d56779f22ba
Ade <79329667+adedolapoadegboye@users.noreply.github.com>
2024-10-04 23:05:02 -0700

    env setup

--- main.c
+++ main.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 int main() {
-    char username[1000];
 
+    // Define variables and limits
+    char username[1000];
+    bool validInput =  false;
+
+    do{
+
+    // Get basic user info for personalization
     printf("Enter your username: ");
     scanf("%s", username);
-
     printf("Hello %s!.\nWelcome to the Area and Perimeter Calculator\n", username);
+    validInput = true;
+
+    }
+    
+    while(validInput);
+
+    return 0;
 }
