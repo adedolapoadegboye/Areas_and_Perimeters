b90fa0d9de4596433c087072cf7781af1366fbbb
Ade <79329667+adedolapoadegboye@users.noreply.github.com>
2024-10-05 00:03:43 -0700

    Shape select

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 int main() {
 
     bool validInput;
+    char shapeSelection;
 
     do {
 
@@ -16,44 +17,50 @@ int main() {
     validInput = scanf("%s", username) == 1 && strlen(username) <= 100;
 
     if (!validInput){
-       printf("Invalid username. Please try again.\n");
+       printf("\nInvalid username. Please try again.\n");
     }
     else {
-        printf("Hello %s!\nWelcome to the Area and Perimeter Calculator\n", username);    }
+        printf("\nHello %s!\nWelcome to the Area and Perimeter Calculator\n", username);    }
 
     }
 
     while(!validInput);
 
-    
+
 
     // Get shape type
     do
     {
-    enum shapeType { square = 0, rectangle = 1, circle = 2, triangle = 3, quit = 4 };
     int shape;
-    const char *shapes[] = { "square", "rectangle", "circle", "triangle", "quit" }; 
+    char *shapes[] = { "Square", "Rectangle", "Circle", "Triangle" }; 
 
-    printf("Select a shape from the list: \n");
+    printf("\nSelect a shape from the list: \n");
 
     printf("0. Square\n");
     printf("1. Rectangle\n");
     printf("2. Circle\n");
     printf("3. Triangle\n");
-    printf("4. Quit\n");
 
-    validInput = scanf("%i", &shape) == 1 && shape >= 0 && shape <= 4;
+    validInput = scanf("%i", &shape) == 1 && shape >= 0 && shape <= 3;
 
     if (validInput)
     {
-        printf("Your selection: %s\n", shapes[shape]);
+        printf("\nYour selection: %s\n", shapes[shape]);
+        shapeSelection = shapes[shape][0];
     } else {
-        printf("Invalid selection. Please try again.\n");
+        printf("\nInvalid selection. Please try again.\n");
     }
 
     } while(!validInput);
 
-    
 
+    // Get shape dimensions
+    do
+    {
+    printf("%c \n", shapeSelection);
+    } 
+    while (!validInput);
+
+    
     return 0;
 }
